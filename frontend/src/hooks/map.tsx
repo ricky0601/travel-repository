d4a9e8d90272d7413cbe6249ff6463c3e8b18cb0
1dg <ricky2425@naver.com>
2025-04-13 19:51:02 +0900

    feat: 카카오맵 연결

--- frontend/src/hooks/map.tsx
+++ frontend/src/hooks/map.tsx
@@ -1,12 +1,12 @@
 import { Map, MapMarker } from 'react-kakao-maps-sdk';
 
 const KakaoMap = () => {
-    const s_lat = 37.55465000468857;
-    const s_lng = 126.97059787494679;
+    const s_lat = 33.507233;
+    const s_lng = 126.493416;
 
     return (
     <>
-        <Map center={{ lat: s_lat, lng: s_lng }} style={{ width: '100%', height: '100vh' }}>
+        <Map level={8} center={{ lat: s_lat, lng: s_lng }} style={{ width: '100%', height: '100vh' }}>
             <MapMarker position={{ lat: s_lat, lng: s_lng }}></MapMarker>
         </Map>
     </>
